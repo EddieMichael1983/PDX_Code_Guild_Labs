afe8ea95b846a510fcb2b772e1eae42866b76a54
EddieMichael1983 <46540863+EddieMichael1983@users.noreply.github.com>
2019-04-02 20:21:03 -0700

    Update avg_nums_v2.py

--- avg_nums_v2.py
+++ avg_nums_v2.py
@@ -1,17 +1,18 @@
 nums = [] #sets up list of numbers
 
 while True:    #user enters values until done
+    
     value = input('Enter a number, or done: ')
     if value == 'done':
         break
-    nums.append(int(value))
+    nums.append(float(value))   #users entries to list each time
+                                #use float instead of int to account for decimal points
 
 print('You entered: ' + str(nums))
 
 runningSum = 0  #defines a variable called runningSum
-
 for num in nums:  #using a for loop to iterate over every num
-    runningSum += int(num) #adds value 'num' to the running sum
+    runningSum += float(num) #adds value 'num' to the running sum
 print ('The total is: ' + str(runningSum))  #prints the running sum        
 
 average = runningSum / len(nums)   #defines our average of nums as running sum divided by length of list
