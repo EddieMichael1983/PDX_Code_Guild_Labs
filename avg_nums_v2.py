327d2366a3469787bafd771962ee6dade02e5aac
EddieMichael1983 <46540863+EddieMichael1983@users.noreply.github.com>
2019-04-02 19:57:48 -0700

    Update avg_nums_v2.py

--- avg_nums_v2.py
+++ avg_nums_v2.py
@@ -1,6 +1,6 @@
-nums = []
+nums = [] #sets up list of numbers
 
-while True:
+while True:    #user enters values until done
     value = input('Enter a number, or done: ')
     if value == 'done':
         break
@@ -10,10 +10,12 @@ print('You entered: ' + str(nums))
 
 runningSum = 0  #defines a variable called runningSum
 
-for num in nums:            
-    runningSum += int(num)       
-print ('The total is: ' + str(runningSum))          
+for num in nums:  #using a for loop to iterate over every num
+    runningSum += int(num) #adds value 'num' to the running sum
+print ('The total is: ' + str(runningSum))  #prints the running sum        
 
 average = runningSum / len(nums)   #defines our average of nums as running sum divided by length of list
-print('The average is: ' + str(average))   
+print('The average is: ' + str(average))  #prints the average of the nums
+
+
 
