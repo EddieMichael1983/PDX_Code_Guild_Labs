c24437ebab651fd94f3cebb5454d20dfaa70b5f5
Eddie Michael <eddiemichael1983@gmail.com>
2019-04-02 20:47:48 -0700

    added lab 11

---
+++ calculator.py
@@ -0,0 +1,32 @@
+#calculator.py
+
+operators =['+', "-", "/", "*"]
+
+while True:
+
+    operation = input("What is the operation you'd like to perform?  Please type +, -, /, or *: ")
+
+    if not operation in operators: 
+        print("That's not an operator you can use")
+
+    else:
+
+        num1 = float(input("What is the first number?: "))
+        num2 = float(input("What is the second number?: "))
+
+        if operation == "+":
+
+            print (num1 + num2)
+
+        elif operation == "-":
+
+            print (num1 - num2)
+
+        elif operation == "/":
+
+            print (num1 / num2)
+
+        elif operation == "*":
+
+            print (num1 * num2) 
+
